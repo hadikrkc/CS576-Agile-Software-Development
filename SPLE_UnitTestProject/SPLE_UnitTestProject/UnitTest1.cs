c370028a7011d2701955387425b67c144de23db7
brcoztn <burcu1988-1988@hotmail.com>
2016-11-05 12:25:51 +0300

    Test Project
    Unit Test Project added.

---
+++ SPLE_UnitTestProject/SPLE_UnitTestProject/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SPLE_UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
