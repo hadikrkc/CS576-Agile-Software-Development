df4e7b949d06675fe2f87c6cfda4372943d04671
gonuldsk <gonul3534@hotmail.com>
2016-11-16 12:53:16 +0300

    FileProcess Classının test classı oluşturuldu

---
+++ SPLEModeLUpdateTool/SPLEModelUpdateToolTest/SPLEModelUpdateToolTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SPLEModelUpdateToolTest
+{
+    [TestClass]
+    public class SPLEModelUpdateToolTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+
+        }
+    }
+}
