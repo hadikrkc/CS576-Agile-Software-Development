8d474108b3ea8d2e4b86dfaba165b3b21a1455b4
hadikrkc <hadi.kirkici@gmail.com>
2016-11-17 14:14:07 +0300

    Test Classları oluşturuldu
    Mcm test Classı oluşturuldu.

---
+++ SPLEModeLUpdateTool/SPLEModelUpdateTool.Tests/McmParserTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SPLEModelUpdateTool.Tests
+{
+    [TestClass]
+    public class McmParserTest
+    {
+        [TestMethod]
+        public void Test_WriteXmlNewValues()
+        {
+        }
+        [TestMethod]
+        public void Test_XmlParsing()
+        {
+        }
+    }
+}
